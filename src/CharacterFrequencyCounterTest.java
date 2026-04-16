a977011bb7ecc0c2a449e51048b64e9659224114
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-04-16 11:12:37 -0700

    Worked on testing frequencies

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,23 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    void testCharacterShowsUpMultipleTimesGivesCorrectFrequency() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        // Act
+        int actual = counter.getFrequency('l');
+        // Assert
+        assertEquals(2, actual);
+    }
+
+    @Test
+    void testGetRelativeFrequencyReturns0ForMissingCharacter() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hello");
+        // Act
+        double actual = counter.getRelativePercentage('x');
+        // Assert
+        assertEquals(0.0, actual, 0.0001);
+    }
 }
